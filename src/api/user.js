df6d286b6ba4f78080b43db3a3b8b4946721fdfc
AndoniainaNantenaina <ando.andoniaina.nantenaina@esti.mg>
2023-06-22 13:22:25 +0300

    Ajout de la fonction de synchronisation (local/remote) de PouchDB.

--- src/api/user.js
+++ src/api/user.js
@@ -1,14 +1,48 @@
+// Fichier : user.js
+// Description : Ce fichier contient les fonctions pour gérer les utilisateurs dans la base de données
+
 import PouchDB from 'pouchdb'
 
-export const userDB = new PouchDB('http://localhost:5984/pwa-db/');
+export const localUserDB = new PouchDB('pwa-db');
+export const userDB = new PouchDB('http://localhost:5984/users/');
 
+// Appel de la fonction de synchronisation entre la base de données local et distante
+PouchDB.sync(localUserDB, userDB, {
+    live: true,
+    retry: true
+});
+
+// Fonction pour ajouter un utilisateur dans la base de données
 export const createUser = async (user) => {
-    await userDB.post(user)
-    .then((res) => {
-        console.log(res.id);
-    })
+
+    // Vérification si l'utilisateur existe déjà
+    const check = checkUser(user.email);
+
+    if (check) {
+        alert("Cet utilisateur existe déjà");
+        return;
+    }
+    else {
+        // Ajout de l'utilisateur dans la base de données
+        await userDB.post(user)
+        .then((res) => {
+            console.log(res.id);
+        })
+    }
 }
 
+// Fonction pour vérifier si un utilisateur existe dans la base de données
+export const checkUser = async (userEmail) => {
+    const res = await userDB.find({
+        selector: {
+            email: userEmail
+        }
+    })
+
+    return res.docs.length > 0;
+}
+
+// Fonction pour prendre un utilisateur dans la base de données à partir de son iD
 export const getUser = (id) => {
     return userDB.get(id);
 }
--- src/pages/register.js
+++ src/pages/register.js
@@ -8,39 +8,68 @@ export default function Register() {
     const [password, setPassword] = useState('');
     const [passwordConfirm, setPasswordConfirm] = useState('');
 
+    const [loading, setLoading] = useState(false);
+
     const handleRegister = async () => {
-        await createUser({
-            username: username,
-            email: email,
-            password: password
-        })
-        .then((res) => {
-            console.log("User created");
-        })
+        setLoading(true);
+
+        if (password !== passwordConfirm) {
+            alert("Les mots de passe ne correspondent pas");
+            setLoading(false);
+            return;
+        }
+        
+        else {
+            await createUser({
+                username: username,
+                email: email,
+                password: password
+            })
+            .then((res) => {
+                console.log("User created");
+            })
+        }
+
+        setLoading(false);
     }
 
     return(
-        <div>
-            <h1>Créer un compte</h1>
-            
-            <label htmlFor="username">Nom d'utilisateur</label>
-            <input type="text" id="username"
-            onChange={(e) => setUsername(e.target.value)} />
+        <div className="h-screen w-screen bg-slate-800 text-white flex flex-row items-center">
+            <div className="grid grid-flow-row justify-center w-full">
+                <div className="flex flex-col items-center w-auto gap-2 bg-slate-400 text-white p-4 rounded-lg">
+                    <h1
+                    className="text-2xl font-bold"
+                    >Créer un compte</h1>
+                    
+                    <label htmlFor="username">Nom d'utilisateur</label>
+                    <input type="text" id="username" placeholder="John Doe"
+                    className="p-2 rounded-md bg-slate-700 text-white"
+                    onChange={(e) => setUsername(e.target.value)} />
 
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email"
-            onChange={(e) => setEmail(e.target.value)} />
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" placeholder="example@email.com"
+                    className="p-2 rounded-md bg-slate-700 text-white"
+                    onChange={(e) => setEmail(e.target.value)} />
 
-            <label htmlFor="password">Mot de passe</label>
-            <input type="password" id="password"
-            onChange={(e) => setPassword(e.target.value)} />
-            
-            <label htmlFor="password-confirm">Confirmer le mot de passe</label>
-            <input type="password" id="password-confirm"
-            onChange={(e) => setPasswordConfirm(e.target.value)} />
+                    <label htmlFor="password">Mot de passe</label>
+                    <input type="password" id="password" placeholder="********"
+                    className="p-2 rounded-md bg-slate-700 text-white"
+                    onChange={(e) => setPassword(e.target.value)} />
+                    
+                    <label htmlFor="password-confirm">Confirmer le mot de passe</label>
+                    <input type="password" id="password-confirm" placeholder="********"
+                    className="p-2 rounded-md bg-slate-700 text-white"
+                    onChange={(e) => setPasswordConfirm(e.target.value)} />
 
-            <button
-            onClick={handleRegister}>Créer un compte</button>
+                    <button
+                    className="bg-blue-800 p-2 rounded-md w-full"
+                    onClick={handleRegister}>
+                        {
+                            loading ? "Création" : "Créer un compte"
+                        }
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
